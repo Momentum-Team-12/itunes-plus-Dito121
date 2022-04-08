204e1937abf41409712017fb26227d71905fd1d5
Gerardo Ayes <gerardoayes92@gmail.com>
2022-04-08 11:48:17 -0400

    working on formatting release dates

---
+++ functions.js
@@ -0,0 +1,13 @@
+// functions stored here 
+
+function buildElement(elementType, className, text) {
+    let element = document.createElement(elementType)
+    element.classList.add(className)
+    element.innerText = text
+    return element
+}
+
+function reformatReleaseDate(string) {
+    return string.slice()
+}
+
--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let songDiv = document.getElementById('content')
+let songDiv = document.getElementById('content') // this div contains all search results
 
-fetch("https://itunes.apple.com/search?term=jdilla&media=music", {
+fetch("https://proxy-itunes-api.glitch.me/search?term=radiohead&media=music", {
     method: 'GET',
     headers: {},
 })
@@ -9,23 +9,29 @@ fetch("https://itunes.apple.com/search?term=jdilla&media=music", {
 })
 .then(function(data) {
     for (let result of data.results){
-        songCard = buildElement('div', 'songCard', '')
-        songName = buildElement('div', 'songName', result.trackName)
-        pic = buildElement('img', 'picture', '')
-        pic.src = result.artworkUrl100
-        artistName = buildElement('div', 'artistName', result.artistName)
 
-        songCard.appendChild(pic)
-        songCard.appendChild(songName)
-        songCard.appendChild(artistName)
-        songDiv.appendChild(songCard)
+        songCard = buildElement('div', 'songCard', '') 
+        // this div-type element is the container for all song info
+        songName = buildElement('div', 'songName', result.trackName) 
+        songName.id = result.trackName
+        // this div-type element is for the song name
+        artistName = buildElement('div', 'artistName', result.artistName) 
+        artistName.id = result.artistName
+        // this div-type element is for the artist name
+        pic = buildElement('img', 'picture', '') 
+        // this img-type element is for the album cover
+        pic.src = result.artworkUrl100.slice(0, -13) + "200x200bb.jpg"
+        // this sets source url of album cover to appropriate picture
+        releaseDate = buildElement('div', 'picture', result.releaseDate) 
+
+        songCard.appendChild(pic) 
+        // this puts picture of album cover into song card so that it is on the top
+        songCard.appendChild(songName) 
+        // this puts song name into song card so that it is directly under the image
+        songCard.appendChild(artistName) 
+        // this puts artist name into song card so that it is below song name
+        songDiv.appendChild(songCard) 
+        // this puts song card with picture and info into the search results area
     }
 })
 
-// functions stored below here
-function buildElement(elementType, className, text) {
-    let element = document.createElement(elementType)
-    element.classList.add(className)
-    element.innerText = text
-    return element
-}
