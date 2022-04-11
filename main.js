0e5073ec6e83979a354700d1d8526597d171745a
Gerardo Ayes <gerardoayes92@gmail.com>
2022-04-11 13:26:49 -0400

    everything working. next need to add audio element on each song card

--- main.js
+++ main.js
@@ -1,66 +1,71 @@
-let searchResultsDiv = document.getElementById('searchResultsDiv') 
-// this div contains all search results
-let quickfindDiv = document.getElementById('quickfindDiv')
-let quickfindOptions = document.getElementById('quickfindOptions')
-//let apiSearch = "https://proxy-itunes-api.glitch.me/search?term=" + userInput + "&media=music"
+let searchForm = document.getElementById('searchForm')
 
-fetch("https://proxy-itunes-api.glitch.me/search?term=arctic+monkeys&media=music", {
-    method: 'GET',
-    headers: {},
-})
-.then(function(response) {
-    return response.json()
-})
-.then(function(data) {
+searchForm.addEventListener('submit', function(event){
+    event.preventDefault()
 
-    let quickfindSelect = document.createElement("select")
-    quickfindSelect.id = "quickfindSelect"
-    
-    let labelElement = document.createElement("label")
-    let label = document.createElement("div")
-    label.innerText = "Quick Find: "
-    labelElement.appendChild(label)
+    let searchResultsDiv = document.getElementById('searchResultsDiv') 
+    // this div contains all search results
+    let quickfindDiv = document.getElementById('quickfindDiv')
+    let quickfindOptions = document.getElementById('quickfindOptions')
 
-    quickfindOptions.appendChild(label).appendChild(quickfindSelect)
+    let searchValue = document.getElementById('search').value
+    let apiSearch = `https://proxy-itunes-api.glitch.me/search?term=${searchValue}&media=music`
 
-    for (let result of data.results){
+    fetch(apiSearch, {
+        method: 'GET',
+        headers: {},
+    })
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data) {
 
-        let songCard = buildElement('div', 'songCard', '') 
-        songCard.id = result.trackName
-        // this div-type element is the container for all song info
-        let artistName = buildElement('div', 'artistName', result.artistName) 
-        artistName.id = result.artistName
-        // this div-type element is for the artist name
-        let songName = buildElement('div', 'songName', result.trackName) 
-        //songName.id = result.trackName
-        // this div-type element is for the song name
-        let releaseDate = buildElement('div', 'releaseDate', reformatReleaseDate(result.releaseDate))
-        // this div-type element is for the release
-        let pic = buildElement('img', 'albumCover', '') 
-        // this img-type element is for the album cover
-        pic.src = result.artworkUrl100.slice(0, -13) + "200x200bb.jpg"
-        // this sets source url of album cover to appropriate picture
+        var quickfindSelect = document.getElementById('quickfindSelect')
+        quickfindSelect.remove()
 
-        songCard.appendChild(pic) 
-        // this puts picture of album cover into song card so that it is on the top
-        songCard.appendChild(artistName) 
-        // this puts artist name into song card so that it is below song name
-        songCard.appendChild(songName) 
-        // this puts song name into song card so that it is directly under the image
-        songCard.appendChild(releaseDate)
-        // this puts release date of song into song card so that it is at bottom
-        searchResultsDiv.appendChild(songCard) 
-        // this puts song card with picture and info into the search results area
+        var quickfindSelect = document.createElement("select")
+        quickfindSelect.id = "quickfindSelect"
 
-        let option = document.createElement("option")
-        option.value = result.trackName
-        option.innerText = result.trackName
-        quickfindSelect.appendChild(option)
-        // this populates dropdown menu with search results for easy search selection
-    }
+        quickfindOptions.appendChild(quickfindSelect)
 
-    quickfindDiv.addEventListener("submit", function(event) {
-    document.location = `#${quickfindSelect.options[quickfindSelect.selectedIndex].value}`
+        for (let result of data.results){
+
+            let songCard = buildElement('div', 'songCard', '') 
+            songCard.id = result.trackName
+            // this div-type element is the container for all song info
+            let artistName = buildElement('div', 'artistName', result.artistName) 
+            artistName.id = result.artistName
+            // this div-type element is for the artist name
+            let songName = buildElement('div', 'songName', result.trackName) 
+            //songName.id = result.trackName
+            // this div-type element is for the song name
+            let releaseDate = buildElement('div', 'releaseDate', reformatReleaseDate(result.releaseDate))
+            // this div-type element is for the release
+            let pic = buildElement('img', 'albumCover', '') 
+            // this img-type element is for the album cover
+            pic.src = result.artworkUrl100.slice(0, -13) + "200x200bb.jpg"
+            // this sets source url of album cover to appropriate picture
+
+            songCard.appendChild(pic) 
+            // this puts picture of album cover into song card so that it is on the top
+            songCard.appendChild(artistName) 
+            // this puts artist name into song card so that it is below song name
+            songCard.appendChild(songName) 
+            // this puts song name into song card so that it is directly under the image
+            songCard.appendChild(releaseDate)
+            // this puts release date of song into song card so that it is at bottom
+            searchResultsDiv.appendChild(songCard) 
+            // this puts song card with picture and info into the search results area
+
+            let option = buildElement("option", "option", result.trackName)
+            option.value = result.trackName
+            quickfindSelect.appendChild(option)
+            // this populates dropdown menu with search results for easy search selection
+        }
+
+        quickfindDiv.addEventListener("submit", function(event) {
+            event.preventDefault()
+            document.location = `#${quickfindSelect.options[quickfindSelect.selectedIndex].value}`
+        })
     })
 })
-
